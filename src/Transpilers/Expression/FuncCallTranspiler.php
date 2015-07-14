0eef2b49bfef6662baa2e64b5745ea3d744920b1
Avi Block <atblock@gmail.com>
2015-07-14 08:43:53 -0400

    Allow passing in function implementations into the configuration

--- src/Transpilers/Expression/FuncCallTranspiler.php
+++ src/Transpilers/Expression/FuncCallTranspiler.php
@@ -11,13 +11,17 @@ class FuncCallTranspiler extends AbstractTranspiler
      */
     public function transpile()
     {
-        $functionName = $this->node->name;
+        $functionName = $this->node->name->toString();
         $args = array();
         foreach($this->node->args as $arg) {
         	$dispatcher = new NodeDispatcher($arg->value);
         	$dispatcher->setContext($this);
 
         	$args[] = $dispatcher->dispatch();
+        }
+
+        if (isset($this->configuration->functions[$functionName])) {
+            return $this->configuration->functions[$functionName]($args);
         }
 
         return $functionName.'('.(implode(',',$args)).')';
